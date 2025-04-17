03316e7be55365337b743aca548b856946b40b3f
Marko Divjak <markodivjak@microsoft.com>
2025-04-17 14:42:37 +0200

    feat: implement ETWExporter struct and resource management

--- opentelemetry-etw-trace/src/exporter.rs
+++ opentelemetry-etw-trace/src/exporter.rs
@@ -1,6 +1,26 @@
 //! ETW Trace Exporter implementation
 
-// Exporter struct and basic skeleton will be defined here.
+use std::fmt::Debug;
+use std::pin::Pin;
+use std::sync::Arc;
+use tracelogging_dynamic as tld;
+
+#[derive(Default)]
+pub(crate) struct Resource {
+    pub cloud_role: Option<String>,
+    pub cloud_role_instance: Option<String>,
+}
+
+pub struct ETWExporter {
+    pub(crate) provider: Pin<Arc<tld::Provider>>,
+    pub(crate) resource: Resource,
+}
+
+impl Debug for ETWExporter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("ETW trace exporter")
+    }
+}
 
 pub struct EtwTraceExporter {
     // Fields for configuration and state will go here
